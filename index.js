b56a042c003153f0b4e47f03ee7cdc4706f0cf02
KgHtetNaing <50042055+KgHtetNaing@users.noreply.github.com>
2020-01-21 12:38:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,34 +4,36 @@
 const
     express = require('express'),
     bodyParser = require('body-parser'),
+    requestify = require('requestify'),
     app = express().use(bodyParser.json()); // creates express http server
+
+const pageaccesstoken = 'EAAihIcIsCAgBAGQ1yAr1BXbayDXVGesKXamXMBdEwaMzjkfHT5hZAHZBtUYrc8hjEQiVzFin8eb4NBK9tuOBFub7WAVwXCMl2L5AsqbD1OjG3Dh99IsYeMjZCLiYLcZAFG0fo5CZA6YCODRe1OCLsE21OTRZCXDmGknF9HBdSKSAZDZD'
+
+requestify.post(`https://graph.facebook.com/v2.6/me/messenger_profile?access_token=${pageaccesstoken}`,
+    {
+        "get_started": {
+            "payload": "Hi"
+        },
+        "greeting": [
+            {
+                "locale": "default",
+                "text": "Hello {{user_first_name}}!"
+            }, {
+                "locale": "en_US",
+                "text": "Timeless apparel for the masses."
+            }
+        ]
+    }
+).then(response => {
+    console.log(response)
+}).fail(error => {
+    console.log(error)
+})
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-app.post('/webhook', (req, res) => {
 
-    let body = req.body;
-
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
-
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-
-});
+// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
@@ -57,4 +59,53 @@ app.get('/webhook', (req, res) => {
             res.sendStatus(403);
         }
     }
+});
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {
+
+    let body = req.body;
+
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
+
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+            if (webhook_event.message) {
+                var userInput = webhook_event.message.text
+            }
+            if (webhook_event.postback) {
+                var userButton = webhook_event.postback.payload
+            }
+            if (userInput == 'Hi' || userButton == 'Hi') {
+                let welcomeMessage = {
+                    "recipient": {
+                        "id": webhook_event.sender.id
+                    },
+                    "message": {
+                        "text": "Hello, Welcome to Online First Aid!"
+                    }
+                }
+                requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`,
+                    welcomeMessage
+                ).then(response => {
+                    console.log(response)
+                }).fail(error => {
+                    console.log(error)
+                })
+            }
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+
 });
