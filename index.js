13935b652486f728d7aad919cf1a41ffee4e1fe3
KgHtetNaing <50042055+KgHtetNaing@users.noreply.github.com>
2020-01-28 12:16:28 -0800

    ab

--- index.js
+++ index.js
@@ -99,19 +99,10 @@ app.post('/webhook', (req, res) => {
                     console.log(error)
                 })
             }
-            "payload": {
-                "template_type": "button",
-                    "text": "<Hi>",
-                        "buttons": [
-                            <1>,
-    <2>, 
-                                ...
-                              ]
-                            }
-                                    });
-                            
-                                    // Returns a '200 OK' response to all requests
-                                    res.status(200).send('EVENT_RECEIVED');
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
