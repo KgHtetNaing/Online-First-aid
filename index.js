edbde4efe833e2e90b85c018df0b7b30d24415f1
KgHtetNaing <50042055+KgHtetNaing@users.noreply.github.com>
2020-01-30 12:40:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -360,14 +360,16 @@ app.post('/webhook', (req, res) => {
                         "text":"welcome"
                     }
 
-                    requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
+
+            }
+
+        requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
                 ).then(response => {
                     console.log(response)
                 }).fail(error => {
                     console.log(error)
                 })
-            
-            }}
+            }
 
              
 
