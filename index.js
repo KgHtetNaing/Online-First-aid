3b64e7f0a1e56fafe779a17e866e04538dd06bd3
KgHtetNaing <50042055+KgHtetNaing@users.noreply.github.com>
2020-01-30 14:09:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ requestify.post(`https://graph.facebook.com/v2.6/me/messenger_profile?access_tok
                 "text": "Hello {{user_first_name}}!"
             }, {
                 "locale": "en_US",
-                "text": "You can either choose from the given selection or type the availabe selection."
+                "text": "Timeless apparel for the masses."
             }
         ]
     }
@@ -357,8 +357,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":"These are the steps for curing the nose bleeding. 1. Sit Upright and lean forward. 2. Do not pack the nose.3. Use decongestant (eg. breathing steam, placing a wet warm towel) 4. Pinch the part of the nose below the nasal bones for about 10 minutes."
-                        "text": "Sit Upright"
+                        "text":"These are the steps for cuuring the nose bleeding. 1. Sit Upright and lean forward. 2. Do not pack the nose.3. Use decongestant (eg. breathing steam, placing a wet warm towel) 4. Pinch the part of the nose below the nasal bones for about 10 minutes."
                     }
 
 
@@ -368,9 +367,7 @@ app.post('/webhook', (req, res) => {
                     console.log(response)
                 }).fail(error => {
                     console.log(error)
-                })  //nosebleeding end
-
-           
+                })  //nosebleed
             
 
         }
