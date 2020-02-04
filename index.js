765af4ac8ef1fe476dcf40e9c511fa4fb3072990
KgHtetNaing <50042055+KgHtetNaing@users.noreply.github.com>
2020-02-04 11:25:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -435,7 +435,7 @@ app.post('/webhook', (req, res) => {
                                 "template_type": "generic",
                                 "elements": [
                                     {
-                                        "title": "Choose your injury type!",
+                                        "title": "Choose your injury !",
                                         "image_url": "https://petersfancybrownhats.com/company_image.png",
                                         "subtitle": "Online First-aid in your service.",
                                         "default_action": {
@@ -456,7 +456,7 @@ app.post('/webhook', (req, res) => {
 
 
                                     {
-                                        "title": "Welcome!",
+                                        "title": "Choose your injury!",
                                         "image_url": "https://petersfancybrownhats.com/company_image.png",
                                         "subtitle": "Online First-aid in your service.",
                                         "default_action": {
@@ -474,7 +474,7 @@ app.post('/webhook', (req, res) => {
                                     },
 
                                     {
-                                        "title": "Welcome!",
+                                        "title": "Choose your injury!",
                                         "image_url": "",
                                         "subtitle": "Online First-aid in your service.",
                                         "default_action": {
@@ -585,7 +585,7 @@ app.post('/webhook', (req, res) => {
                                 "template_type": "generic",
                                 "elements": [
                                     {
-                                        "title": "Choose your injury type!",
+                                        "title": "Choose your injury !",
                                         "image_url": "https://petersfancybrownhats.com/company_image.png",
                                         "subtitle": "Online First-aid in your service.",
                                         "default_action": {
@@ -606,7 +606,7 @@ app.post('/webhook', (req, res) => {
 
 
                                     {
-                                        "title": "Welcome!",
+                                        "title": "Choose your injury!",
                                         "image_url": "https://petersfancybrownhats.com/company_image.png",
                                         "subtitle": "Online First-aid in your service.",
                                         "default_action": {
@@ -623,26 +623,10 @@ app.post('/webhook', (req, res) => {
                                         ]
                                     },
 
-                                    {
-                                        "title": "Welcome!",
-                                        "image_url": "",
-                                        "subtitle": "Online First-aid in your service.",
-                                        "default_action": {
-                                            "type": "web_url",
-                                            "url": "https://petersfancybrownhats.com/view?item=103",
-                                            "webview_height_ratio": "tall",
-                                        },
-                                        "buttons": [
-                                            {
-                                                "type": "postback",
-                                                "title": "Swallowed Poison",
-                                                "payload": "Swallowed Poison"
-                                            }
-                                        ]
-                                    },
+                                   
 
                                      {
-                                        "title": "Welcome!",
+                                        "title": "Choose your injury!",
                                         "image_url": "",
                                         "subtitle": "Online First-aid in your service.",
                                         "default_action": {
@@ -673,7 +657,75 @@ app.post('/webhook', (req, res) => {
                     console.log(error)
                 })
 
+            }//Poisontype end
+
+             else if(userInput == 'Drug Toxicity' || userButton == 'Drug Toxicity')
+             {
+                let buttonMesage = {
+                    "recipient": {
+                        "id": webhook_event.sender.id
+                    },
+                    "message":{
+                        "text":`These are the steps for treating drug toxicity.\n\n1.First,place the patient on their side in the recovery position.\n\n2.Ensure that the airway remain open by tilting head back and lifting chin.\n\n3.If the drug toxcity is due to overdose,try pumping the stoamch to remove the drugs	which have not been absorbed. Activated charcoal may be given to prevent the drugs from being absorbed into the blood.\n\n4.Bring the pill containers to the hospital.`
+                    }
+
+
             }
+            requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
+                ).then(response => {
+                    console.log(response)
+                }).fail(error => {
+                    console.log(error)
+                })  
+            
+
+        }//drugtoxicity end
+
+         else if(userInput == 'Poison in the eye' || userButton == 'Poison in the eye')
+             {
+                let buttonMesage = {
+                    "recipient": {
+                        "id": webhook_event.sender.id
+                    },
+                    "message":{
+                        "text":`These are the steps for treating poison in the eye.\n\n1.Flush the eye with luckewarm water for about 15 to 30 minutes. Have the eyes rinsed or eyes under a faucet in a gentle shower or with a clean container of tower.\n\n2.The patient should keep the eyes as wide as possible.\n\n3.Do not rub the eyes or place bandages over the eye.\n\n4.The patient should wear sunglasses to reduce light sensitivity as much as possible before the medical care arrive\n\n5.Make sure to know what chemical got into the eye so the medical team can give treatment.`
+                    }
+
+
+            }
+            requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
+                ).then(response => {
+                    console.log(response)
+                }).fail(error => {
+                    console.log(error)
+                })  
+            
+
+        }//poisonintheeye end
+
+        else if(userInput == 'Bug Bite' || userButton == 'Bug Bite')
+             {
+                let buttonMesage = {
+                    "recipient": {
+                        "id": webhook_event.sender.id
+                    },
+                    "message":{
+                        "text":`These are the steps for treating the bug bite.\n\n1.Remove the tick, stings or hair if still there.\n\n2.Washed the affected area with soap and water.\n\n3.Apply the cold compress or an icepack to the affected area for at least 10 minutes.\n\n4.Raise the affected area if possible, it can helps in reducing the swelling.\n\n5.Prevent from scratching or bursting any blisters to reduce the risk of infection.\n\n6.Home remedies like vinegar and bicarbonate of soda should not be use as they are unlikely to help.`
+                    }
+
+
+            }
+            requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
+                ).then(response => {
+                    console.log(response)
+                }).fail(error => {
+                    console.log(error)
+                })  
+            
+
+        }
+
+            
 
 
 
