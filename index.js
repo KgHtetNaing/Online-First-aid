f6fde310f7753363ca45d6fd6898df5b37568e69
KgHtetNaing <50042055+KgHtetNaing@users.noreply.github.com>
2020-02-05 22:20:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -172,92 +172,22 @@ app.post('/webhook', (req, res) => {
                     "recipient": {
                         "id": webhook_event.sender.id
                     },
-                    "message":{
-                    	"attachment": {
-                            "type": "template",
-                            "payload": {
-                                "template_type": "generic",
-                                "elements": [
-                                    {
-                                        "title": "Choose your injury type!",
-                                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                                        "subtitle": "Online First-aid in your service.",
-                                        "default_action": {
-                                            "type": "web_url",
-                                            "url": "https://petersfancybrownhats.com/view?item=103",
-                                            "webview_height_ratio": "tall",
-                                        },
-                                        "buttons": [
-                                            {
-                                                "type": "postback",                                               
-                                                "title": "Bleeding",
-                                                "payload": "Bleeding"
-                                            },
-
-                                             
-                                        ]
-                                    },
-
-
-                                   {
-                                        "title": "Choose your injury type!",
-                                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                                        "subtitle": "Online First-aid in your service.",
-                                        "default_action": {
-                                            "type": "web_url",
-                                            "url": "https://petersfancybrownhats.com/view?item=103",
-                                            "webview_height_ratio": "tall",
-                                        },
-                                        "buttons": [
-                                            {
-                                                "type": "postback",
-                                                "title": "Burnt",
-                                                "payload": "Burnt"
-                                            }
-                                        ]
-                                    },
-
-                                    {
-                                        "title": "Choose your injury type!",
-                                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                                        "subtitle": "Online First-aid in your service.",
-                                        "default_action": {
-                                            "type": "web_url",
-                                            "url": "https://petersfancybrownhats.com/view?item=103",
-                                            "webview_height_ratio": "tall",
-                                        },
-                                        "buttons": [
-                                            {
-                                                "type": "postback",
-                                                "title": "Poison",
-                                                "payload": "Poison"
-                                            }
-                                        ]
-                                    },
-
-                                    {
-                                        "title": "Choose your injury type!",
-                                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                                        "subtitle": "Online First-aid in your service.",
-                                        "default_action": {
-                                            "type": "web_url",
-                                            "url": "https://petersfancybrownhats.com/view?item=103",
-                                            "webview_height_ratio": "tall",
-                                        },
-                                        "buttons": [
-                                            {
-                                                "type": "postback",
-                                                "title": "Bone Fracture",
-                                                "payload": "Bone Fracture"
-                                            }
-                                        ]
-                                    },
-                                    
-                                 ]
-                            }
-                        }
-                    
+                   message: { 
+                attachment : {
+                    type : "template",
+                    payload: {
+                        template_type: "button",
+                        
+                        buttons: [
+                        {
+                            type: "postback",
+                            title: "snack",
+                            payload: "snack"
+                        }]
                     }
+                }
+
+            }
                 };
 
                 requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
@@ -377,7 +307,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating the nose bleeding.\n\n1. Sit Upright and lean forward.\n\n2. Do not pack the nose.\n\n3. Use decongestant (eg. breathing steam, placing a wet warm towel)\n\n4. Pinch the part of the nose below the nasal bones for about 10 minutes.`
+                        "text":`These are the steps for treating the nose bleeding before the healthcare arrive.\n\n1. Sit Upright and lean forward.\n\n2. Do not pack the nose.\n\n3. Use decongestant (eg. breathing steam, placing a wet warm towel)\n\n4. Pinch the part of the nose below the nasal bones for about 10 minutes.`
                     }
 
 
@@ -399,7 +329,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating the bleeding wound.\n\n1. Gently clean the wound with soap and warm water.\n\n2. Apply antibiotics and cover the wound with the bandage.\n\n3.Change the bandage daily.`
+                        "text":`These are the steps for treating the bleeding wound before the healthcare arrive.\n\n1. Gently clean the wound with soap and warm water.\n\n2. Apply antibiotics and cover the wound with the bandage.\n\n3.Change the bandage daily.`
                     }
 
 
@@ -421,7 +351,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for stopping the over bleeding.\n\n1. Apply direct pressure on the cut or wound with a clean cloth or tissue.\n\n2. If blood still soaks thorugh material, put more cloth without removing the already applied cloth.\n\n3.If the wound is on the arm or legs, raise it above the heart to slow bleeding.`
+                        "text":`These are the steps for stopping the over bleeding before the healthcare arrive.\n\n1. Apply direct pressure on the cut or wound with a clean cloth or tissue.\n\n2. If blood still soaks thorugh material, put more cloth without removing the already applied cloth.\n\n3.If the wound is on the arm or legs, raise it above the heart to slow bleeding.`
                     }
 
 
@@ -544,7 +474,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating the steam burn.\n\n1.Apply cool (not cold) water over the burn area for about 20 minutes.\n\n2.Use cool compresses (cloth dipped in cool water) if water is not available. Do not use toothpaste.\n\n3.Take pain reliever if necessary.\n\n4.Reduce sun exposure`
+                        "text":`These are the steps for treating the steam burn before the healthcare arrive.\n\n1.Apply cool (not cold) water over the burn area for about 20 minutes.\n\n2.Use cool compresses (cloth dipped in cool water) if water is not available. Do not use toothpaste.\n\n3.Take pain reliever if necessary.\n\n4.Reduce sun exposure`
                     }
 
 
@@ -566,7 +496,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating electric burn.\n\n1.Perform CPR(shown in Emergency section) if the patient is unresponsive.\n\n2.Can not let the patient become chilled.\n\n3.Cover the burn aread with a sterile bandage or clean cloth.Do not use a blanket or towel as loose fiber can stick to the burnt area.`
+                        "text":`These are the steps for treating electric burn before the healthcare arrive.\n\n1.Perform CPR(shown in Emergency section) if the patient is unresponsive.\n\n2.Can not let the patient become chilled.\n\n3.Cover the burn aread with a sterile bandage or clean cloth.Do not use a blanket or towel as loose fiber can stick to the burnt area.`
                     }
 
 
@@ -588,7 +518,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating Chemical burn.\n\n1.Remove the cause of the burn by running the cool water on it for 10 minutes.For dry chemicals, use brush or gloves.\n\n2.Remove clothing or accessory which has been contaminated by the chemical.\n\n3.Bandge the burn with sterile gauze bandage or a clean cloth. Do not use fluffy cotton. Bandge loosely to prevent from putting pressure on the burned skin.\n\n4.If the patient stil feel burn after the flushing, flush the area again with water.`
+                        "text":`These are the steps for treating Chemical burn before the healthcare arrive.\n\n1.Remove the cause of the burn by running the cool water on it for 10 minutes.For dry chemicals, use brush or gloves.\n\n2.Remove clothing or accessory which has been contaminated by the chemical.\n\n3.Bandge the burn with sterile gauze bandage or a clean cloth. Do not use fluffy cotton. Bandge loosely to prevent from putting pressure on the burned skin.\n\n4.If the patient stil feel burn after the flushing, flush the area again with water.`
                     }
 
 
@@ -711,7 +641,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating drug toxicity.\n\n1.First,place the patient on their side in the recovery position.\n\n2.Ensure that the airway remain open by tilting head back and lifting chin.\n\n3.If the drug toxcity is due to overdose,try pumping the stoamch to remove the drugs	which have not been absorbed. Activated charcoal may be given to prevent the drugs from being absorbed into the blood.\n\n4.Bring the pill containers to the hospital.`
+                        "text":`These are the steps for treating drug toxicity before the healthcare arrive.\n\n1.First,place the patient on their side in the recovery position.\n\n2.Ensure that the airway remain open by tilting head back and lifting chin.\n\n3.If the drug toxcity is due to overdose,try pumping the stoamch to remove the drugs	which have not been absorbed. Activated charcoal may be given to prevent the drugs from being absorbed into the blood.\n\n4.Bring the pill containers to the hospital.`
                     }
 
 
@@ -733,7 +663,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating poison in the eye.\n\n1.Flush the eye with luckewarm water for about 15 to 30 minutes. Have the eyes rinsed or eyes under a faucet in a gentle shower or with a clean container of tower.\n\n2.The patient should keep the eyes as wide as possible.\n\n3.Do not rub the eyes or place bandages over the eye.\n\n4.The patient should wear sunglasses to reduce light sensitivity as much as possible before the medical care arrive\n\n5.Make sure to know what chemical got into the eye so the medical team can give treatment.`
+                        "text":`These are the steps for treating poison in the eye before the healthcare arrive.\n\n1.Flush the eye with luckewarm water for about 15 to 30 minutes. Have the eyes rinsed or eyes under a faucet in a gentle shower or with a clean container of tower.\n\n2.The patient should keep the eyes as wide as possible.\n\n3.Do not rub the eyes or place bandages over the eye.\n\n4.The patient should wear sunglasses to reduce light sensitivity as much as possible before the medical care arrive\n\n5.Make sure to know what chemical got into the eye so the medical team can give treatment.`
                     }
 
 
@@ -755,7 +685,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating the bug bite.\n\n1.Remove the tick, stings or hair if still there.\n\n2.Washed the affected area with soap and water.\n\n3.Apply the cold compress or an icepack to the affected area for at least 10 minutes.\n\n4.Raise the affected area if possible, it can helps in reducing the swelling.\n\n5.Prevent from scratching or bursting any blisters to reduce the risk of infection.\n\n6.Home remedies like vinegar and bicarbonate of soda should not be use as they are unlikely to help.`
+                        "text":`These are the steps for treating the bug bite before the healthcare arrive.\n\n1.Remove the tick, stings or hair if still there.\n\n2.Washed the affected area with soap and water.\n\n3.Apply the cold compress or an icepack to the affected area for at least 10 minutes.\n\n4.Raise the affected area if possible, it can helps in reducing the swelling.\n\n5.Prevent from scratching or bursting any blisters to reduce the risk of infection.\n\n6.Home remedies like vinegar and bicarbonate of soda should not be use as they are unlikely to help.`
                     },
                 
 
@@ -856,7 +786,7 @@ app.post('/webhook', (req, res) => {
                         "id": webhook_event.sender.id
                     },
                     "message":{
-                        "text":`These are the steps for treating broken bone.\n\n1.If the wound is bleeding, try and stop it by using a sterile bandage,a clean cloth or a clean piece of clothing and elevating the injured aread\n\n2.If it is the neck or back which is broken, try to make the patient stay still as much as possible.If it is the limbs which is broken, immobalize using a splint or sling.\n\n3.After that, wrap an ice pack or bag of ice cubes in a cloth and press it onto the injured aread for up to 10 minutes at a time.`
+                        "text":`These are the steps for treating broken bone before the healthcare arrive.\n\n1.If the wound is bleeding, try and stop it by using a sterile bandage,a clean cloth or a clean piece of clothing and elevating the injured aread\n\n2.If it is the neck or back which is broken, try to make the patient stay still as much as possible.If it is the limbs which is broken, immobalize using a splint or sling.\n\n3.After that, wrap an ice pack or bag of ice cubes in a cloth and press it onto the injured aread for up to 10 minutes at a time.\n\n5.If the patient become unconscious or does not appear to be breathing, perform CPR(shown in Emergency)`
                     }
 
 
@@ -869,7 +799,29 @@ app.post('/webhook', (req, res) => {
                 })  
             
 
-        }
+        }//brokenbone end
+
+         else if(userInput == 'Bone Disclocation' || userButton == 'Broken Bone')
+             {
+                let buttonMesage = {
+                    "recipient": {
+                        "id": webhook_event.sender.id
+                    },
+                    "message":{
+                        "text":`These are the steps for treating broken bone before the healthcare arrive.\n\n1.Leave the joint alone and do not attempt to move or jam a dislocated bone as it can destroy blood vessels.\n\n2.Wrap a cloth around the ice and press it around the discolated bone.\n\nUse painkiller if necessary.`
+                    }
+
+
+            }
+            requestify.post(`https://graph.facebook.com/v5.0/me/messages?access_token=${pageaccesstoken}`, buttonMesage
+                ).then(response => {
+                    console.log(response)
+                }).fail(error => {
+                    console.log(error)
+                })  
+            
+
+        } //
 
             
 
